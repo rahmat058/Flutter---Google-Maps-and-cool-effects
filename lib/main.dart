a16c067ffad66568c6eee03a0a60820dee10f39f
rahmat058 <md.kazi.rahmat@gmail.com>
2020-01-16 23:27:06 +0600

    Google maps slide show done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_map/coffee_model.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,36 +10,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Maps',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -44,68 +28,196 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  GoogleMapController _controller;
+  PageController _pageController;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  List<Marker> allMarkers = [];
+
+  int prevPage;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    coffeeShops.forEach((element) {
+      allMarkers.add(Marker(
+        markerId: MarkerId(element.shopName),
+        draggable: false,
+        infoWindow: InfoWindow(title: element.shopName, snippet: element.address),
+        position: element.locationCoords
+      ));
     });
+
+    _pageController = PageController(initialPage: 1, viewportFraction: 0.8)..addListener(_onScroll);
+  }
+
+  void _onScroll() {
+    if(_pageController.page.toInt() != prevPage) {
+      prevPage =_pageController.page.toInt();
+      moveCamera();
+    }
+  }
+
+  _coffeeShopList(index) {
+    return AnimatedBuilder(
+        animation: _pageController,
+        builder: (BuildContext contex, Widget widget) {
+          double value = 1;
+
+          if(_pageController.position.haveDimensions) {
+            value = _pageController.page - index;
+            value = (1-(value.abs() * 0.3) + 0.06).clamp(0.0, 1.0);
+          }
+
+          return Center(
+            child: SizedBox(
+              height: Curves.easeInOut.transform(value) * 250.0,
+              width: Curves.easeInOut.transform(value) * 350.0,
+              child: widget,
+            ),
+          );
+        },
+
+      child: InkWell(
+        onTap: () {
+          moveCamera();
+        },
+        child: Stack(
+          children: <Widget>[
+            Center(
+              child: Container(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 10.0,
+                  vertical: 20.0
+                ),
+                height: 100.0,
+                width: 275.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black54,
+                      offset: Offset(0.0, 4.0),
+                      blurRadius: 10.0
+                    )
+                  ]
+                ),
+
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.white
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        height: 100.0,
+                        width: 100.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(10.0),
+                            topLeft: Radius.circular(10.0)
+                          ),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              coffeeShops[index].thumbNail
+                            ),
+                            fit: BoxFit.contain
+                          )
+                        ),
+                      ),
+
+                      SizedBox(
+                        width: 5.0,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            coffeeShops[index].shopName,
+                            style: TextStyle(fontSize: 12.5, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            coffeeShops[index].address,
+                            style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w600),
+                          ),
+
+                          Container(
+                            width: 170.0,
+                            child: Text(
+                              coffeeShops[index].description,
+                              style: TextStyle(fontSize: 11.0, fontWeight: FontWeight.w300),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Maps"),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height - 50.0,
+            width: MediaQuery.of(context).size.width,
+            child: GoogleMap(
+              initialCameraPosition: CameraPosition(
+                  target: LatLng(40.7128, -74.0060), zoom: 12.0
+              ),
+              markers: Set.from(allMarkers),
+              onMapCreated: mapCreated,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+          ),
+
+          Positioned(
+            bottom: 20.0,
+            child: Container(
+              height: 200.0,
+              width: MediaQuery.of(context).size.width,
+              child: PageView.builder(
+                  controller: _pageController,
+                itemCount: coffeeShops.length,
+                itemBuilder: (BuildContext contex, int index) {
+                    return _coffeeShopList(index);
+                },
+              ),
             ),
-          ],
-        ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void mapCreated(controller) {
+    setState(() {
+      _controller = controller;
+    });
+  }
+
+  moveCamera() {
+    _controller.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+        target: coffeeShops[_pageController.page.toInt()].locationCoords,
+        zoom: 14.0,
+        bearing: 45.0,
+        tilt: 45.0
+      )
+    ));
   }
 }
